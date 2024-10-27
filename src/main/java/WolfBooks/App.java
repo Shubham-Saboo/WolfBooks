e26d642970848ebc3ca47be356d353a6a3b2f176
apatel29 <apatel29@ncsu.edu>
2024-10-27 14:29:58 -0400

    Pushing changes for App.java.

--- src/main/java/WolfBooks/App.java
+++ src/main/java/WolfBooks/App.java
@@ -1,9 +1,22 @@
 package src.main.java.WolfBooks;
+import java.sql.*;
 
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("Testing");
-    }
+        Connection conn = null;
+        Scanner sc = new Scanner(System.in);
+        try {
+            String dbUrl = "jdbc:mysql://localhost:3306/wolfbooks";
+            String dbUser = "root";
+            String dbPass = "";
+            conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
 
+
+
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        sc.close();
+    }
 }
